ef17e6cb15ea756772e79a4712994f399068db6d
gkaguirre <gkaguirre@me.com>
2021-10-14 09:50:21 -0400

    Config

--- code/demoFitdemoData.m
+++ code/demoFitdemoData.m
@@ -1,15 +1,24 @@
 dataPath = fileparts(fileparts(mfilename('fullpath')));
+fileName = 'gkaDemoBlinkCNSData.csv';
+%fileName = 'p97.csv';
 
-T = readtable(fullfile(dataPath,'data','gkaDemoBlinkCNSData.csv'));
+T = readtable(fullfile(dataPath,'data',fileName));
 varNames = T.Properties.VariableNames;
 
 xFit = linspace(log10(4),log10(50),50);
     figure
 
 for ii = 2:length(varNames)
-    subplot(4,3,ii-1);
+    subplot(5,3,ii-1);
     x = log10(T.PSI);
     y = T.(varNames{ii});
+    switch varNames{ii}
+        case 'latency'
+            y = 100-y;
+            titleStr = '100-latency';
+        otherwise
+            titleStr = varNames{ii};
+    end
     goodPoints = ~isnan(y);
     x = x(goodPoints);
     y = y(goodPoints);
@@ -19,7 +28,7 @@ for ii = 2:length(varNames)
     [fitObj,G] = L3P(x,y);
     hold on
     plot(xFit,fitObj(xFit),'-r')
-    xlim(log10([4 50]));
-    title(varNames{ii})
+    xlim(log10([2 70]));
+    title([titleStr sprintf(' R^2=%2.2f',G.rsquare)])
     xlabel('puff pressure [log psi]')
 end
