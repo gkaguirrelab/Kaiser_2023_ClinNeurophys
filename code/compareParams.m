b731a85ca6af4dd7ab20340b857fd33fac4edba1
Briana Haggerty <brianahaggerty@stingray.psych.upenn.edu>
2022-02-17 14:21:18 -0500

    PC1 analysis

--- code/compareParams.m
+++ code/compareParams.m
@@ -17,8 +17,10 @@ varNamesToPlot = {'aucI', 'latencyI', 'timeUnderI', 'openTimeI', 'initVelocityI'
 T = readtable(fullfile(dataPath,'data',spreadsheet));
 allVarNames = T.Properties.VariableNames;
 slopes = zeros(length(subList), length(varNamesToPlot));
+slopesSessOne = zeros(length(subList), length(varNamesToPlot));
+slopesSessTwo = zeros(length(subList), length(varNamesToPlot));
 
-% create slopes matrix containing the slope values for each var and subject
+%% create slopes matrix containing the slope values for each var and subject
 for vv = 1:length(varNamesToPlot)
 
     for ss = 1:length(subList)
@@ -28,8 +30,13 @@ for vv = 1:length(varNamesToPlot)
         scans = scans(ismember(scans.valid,'TRUE'),:);
         ii = find(strcmp(varNamesToPlot{vv},allVarNames));
         weights = scans.numIpsi;
+        dates = unique(scans.scanDate);
+        sessOne = scans(ismember(scans.scanDate,dates(1,1)),:);
+        sessTwo = scans(ismember(scans.scanDate,dates(2,1)),:);
+        weightsSessOne = sessOne.numIpsi;
+        weightsSessTwo = sessTwo.numIpsi;
 
-        % subject parameter data
+        % subject parameter data across sessions
         y = scans.(allVarNames{ii});
         goodPoints = ~isnan(y);
         x = log10(scans.PSI);
@@ -37,10 +44,30 @@ for vv = 1:length(varNamesToPlot)
         y = y(goodPoints);
         [x,idxX]=sort(x);
         y = y(idxX);
-
-        % get slope
         fitObj = fitlm(x,y,'RobustOpts', 'on', 'Weight', weights);
         slopes(ss, vv) = fitObj.Coefficients.Estimate(2);
+        
+        % subject parameter data session 1
+        y = sessOne.(allVarNames{ii});
+        goodPoints = ~isnan(y);
+        x = log10(scans.PSI);
+        x = x(goodPoints);
+        y = y(goodPoints);
+        [x,idxX]=sort(x);
+        y = y(idxX);
+        fitObj = fitlm(x,y,'RobustOpts', 'on', 'Weight', weightsSessOne);
+        slopesSessTwo(ss, vv) = fitObj.Coefficients.Estimate(2);
+        
+        % subject parameter data session 2
+        y = sessTwo.(allVarNames{ii});
+        goodPoints = ~isnan(y);
+        x = log10(scans.PSI);
+        x = x(goodPoints);
+        y = y(goodPoints);
+        [x,idxX]=sort(x);
+        y = y(idxX);
+        fitObj = fitlm(x,y,'RobustOpts', 'on', 'Weight', weightsSessTwo);
+        slopesSessOne(ss, vv) = fitObj.Coefficients.Estimate(2);
 
     end
 
@@ -48,7 +75,7 @@ end
 
 comp = corr(slopes);
 
-% Perform a PCA analysis after standardizing the slope measures
+%% Perform a PCA analysis after standardizing the slope measures
 X = (slopes-mean(slopes))./std(slopes);
 [coeff,score,latent,tsquared,explained,mu] = pca(X);
 figure
@@ -61,3 +88,32 @@ figure
 scatter3(score(:,1),score(:,2),score(:,3),'or')
 axis equal
 xlabel('component 1'); ylabel('component 2'); zlabel('component 3');
+
+%% PC1 correlation between session 1 and session 2
+
+weights = abs(coeff(:,1)');
+
+% get session one scores
+sO = (slopes-mean(slopesSessOne))./std(slopesSessOne);
+[coeff,score,latent,tsquared,explained,mu] = pca(sO, 'VariableWeights', weights);
+xx = score(:,1);
+
+% get session two scores
+sT = (slopes-mean(slopesSessOne))./std(slopesSessTwo);
+[coeff,score,latent,tsquared,explained,mu] = pca(sT, 'VariableWeights', weights);
+yy = score(:,1);
+
+figure();
+scatter(xx,yy)
+fitObj = fitlm(xx,yy,'RobustOpts', 'on');
+hold on
+plot(xx,fitObj.Fitted,'-r')
+rsquare = fitObj.Rsquared.Ordinary;
+if rsquare > 1 || rsquare < 0
+    rsquare = nan;
+end
+title(['PC1 scores by session - ' sprintf(' R^2=%2.2f',rsquare)])
+xlabel(['Session one'])
+ylabel(['Session two'])
+axis square
+ylim(xlim)
--- code/testRetestAnalysis.m
+++ code/testRetestAnalysis.m
@@ -14,9 +14,9 @@ spreadsheet ='UPENN Summary with IPSI Responses_02072022.csv';
 % run subjects 149590, 14589, and 14588 only for highest 3 PSI levels
 subList = {15512, 15507, 15506, 15505, 14596, 14595, 14594, 14593, 14592, 14591, ...
     14590, 14589, 14588};
-varNamesToPlot = {'latencyI'};
-% highestOnly = true;
-highestOnly = false;
+varNamesToPlot = {'latencyI', 'aucI', 'closeTimeI', 'openTimeI'};
+highestOnly = true;
+% highestOnly = false;
 
 xFit = linspace(log10(3),log10(70),50);
 % ylims = {[30 65]};
@@ -142,28 +142,16 @@ for vv = 1:length(varNamesToPlot)
     ylim(xlim);
     axis(pl, 'square');
     
-%     % plot offset test retest values across subjects
-%     subplot(1,2,2);
-%     plot(oX,oY,'ob');
-%     fitObj = fitlm(oX,oY,'RobustOpts', 'on');
-%     hold on
-%     plot(oX,fitObj.Fitted,'-r')
-%     rsquare = fitObj.Rsquared.Ordinary;
-%     if rsquare > 1 || rsquare < 0
-%         rsquare = nan;
-%     end
-%     title([varNamesToPlot{vv} ' offset - ' sprintf(' R^2=%2.2f',rsquare)])
-%     xlabel([varNamesToPlot{vv} ' offset session 1'])
-%     ylabel([varNamesToPlot{vv} ' offset session 2'])
-    
 end
 
-%% 2 by 2 test retest slope plots
+%% 2 by 2 test retest slope or offset plots
 
 figure();
 
 for vv = 1:length(varNamesToPlot)
     
+    oX = [];
+    oY = [];
     pX = [];
     pY = [];
     
@@ -196,6 +184,7 @@ for vv = 1:length(varNamesToPlot)
         weights = sessOne.numIpsi;
         mSize = weights*20;
         fitObj = fitlm(x,y,'RobustOpts', 'on', 'Weight', weights);
+        oX(end+1) = fitObj.Coefficients.Estimate(1);
         pX(end+1) = fitObj.Coefficients.Estimate(2);
 
         % session two data
@@ -209,13 +198,73 @@ for vv = 1:length(varNamesToPlot)
         weights = sessTwo.numIpsi;
         mSize = weights*20;
         fitObj = fitlm(x,y,'RobustOpts', 'on', 'Weight', weights);
+        oY(end+1) = fitObj.Coefficients.Estimate(1);
         pY(end+1) = fitObj.Coefficients.Estimate(2);
     end
     
-    % plot parameter test retest values across subjects
+    % plot offset test retest values across subjects
     pl = subplot(2,2,vv);
-    plot(pX, pY, 'ob', 'MarkerSize', 10);
-    fitObj = fitlm(pX,pY,'RobustOpts', 'on');
+    plot(oX, oY, 'ob', 'MarkerSize', 10);
+    fitObj = fitlm(oX,oY,'RobustOpts', 'on');
+    hold on
+    plot(oX,fitObj.Fitted,'-r')
+    pl.Box = 'off';
+    rsquare = fitObj.Rsquared.Ordinary;
+    if rsquare > 1 || rsquare < 0
+        rsquare = nan;
+    end
+    title([varNamesToPlot{vv} ' offset by session - ' sprintf(' R^2=%2.2f',rsquare)], 'FontSize', 16)
+    xlabel(['Offset'], 'FontSize', 16)
+    ylabel(['Offset'], 'FontSize', 16)
+    ylim(xlim);
+    axis(pl, 'square');
+    
+end
+
+%% slope vs offset
+
+figure();
+
+for vv = 1:length(varNamesToPlot)
+    
+    oY = [];
+    pX = [];
+    
+    for ss = 1:length(subList)
+
+        % find scans for desired subject
+        scans = T(ismember(T.subjectID,subList{ss}),:);
+        scans = scans(ismember(scans.valid,'TRUE'),:);
+
+        % separate scans into a table for each of the sessions
+        if highestOnly
+           A = scans(ismember(scans.intendedPSI, 15),:);
+           B = scans(ismember(scans.intendedPSI, 30),:);
+           C = scans(ismember(scans.intendedPSI, 60),:);
+           scans = vertcat(A, B, C);
+        end
+        ii = find(strcmp(varNamesToPlot{vv},allVarNames));
+
+        % subject data
+        y = scans.(allVarNames{ii});
+        goodPoints = ~isnan(y);
+        x = log10(scans.PSI);
+        x = x(goodPoints);
+        y = y(goodPoints);
+        [x,idxX]=sort(x);
+        y = y(idxX);
+        weights = scans.numIpsi;
+        mSize = weights*20;
+        fitObj = fitlm(x,y,'RobustOpts', 'on', 'Weight', weights);
+        oY(end+1) = fitObj.Coefficients.Estimate(1);
+        pX(end+1) = fitObj.Coefficients.Estimate(2);
+        
+    end
+    
+    % plot offset vs slope test retest values across subjects
+    pl = subplot(2,length(varNamesToPlot)/2,vv);
+    plot(pX, oY, 'ob', 'MarkerSize', 10);
+    fitObj = fitlm(pX,oY,'RobustOpts', 'on');
     hold on
     plot(pX,fitObj.Fitted,'-r')
     pl.Box = 'off';
@@ -223,10 +272,9 @@ for vv = 1:length(varNamesToPlot)
     if rsquare > 1 || rsquare < 0
         rsquare = nan;
     end
-    title([varNamesToPlot{vv} ' slope by session - ' sprintf(' R^2=%2.2f',rsquare)], 'FontSize', 16)
+    title([varNamesToPlot{vv} ' offset vs slope across subjects - ' sprintf(' R^2=%2.2f',rsquare)], 'FontSize', 16)
     xlabel(['Slope'], 'FontSize', 16)
-    ylabel(['Slope'], 'FontSize', 16)
-    ylim(xlim);
+    ylabel(['Offset'], 'FontSize', 16)
     axis(pl, 'square');
     
 end
