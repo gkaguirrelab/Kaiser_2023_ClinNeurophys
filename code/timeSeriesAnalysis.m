bdebb7a26602be891c933dd9a05f179fd6119ea1
Briana Haggerty <brianahaggerty@stingray.psych.upenn.edu>
2022-04-18 15:55:41 -0400

    timeSeriesAnalysis format update

--- code/timeSeriesAnalysis.m
+++ code/timeSeriesAnalysis.m
@@ -4,7 +4,8 @@
 %% set up parameters
 
 % load file path
-location = '/Users/brianahaggerty/Documents/MATLAB/projects/blinkCNSAnalysis/data/iFiles/14591/';
+subject = 14591;
+location = strcat('/Users/brianahaggerty/Documents/MATLAB/projects/blinkCNSAnalysis/data/iFiles/', num2str(subject),'/');
 dir(location);
 ds = tabularTextDatastore(location,'FileExtensions',[".csv"]);
 ds.ReadSize = 'file';
@@ -17,7 +18,11 @@ y = [];
 % loop through scan files
 for ii = 1:52
    % make table from file
-   T = read(ds);
+   [data,info] = read(ds);
+   T = data;
+   subStr = eraseBetween(info.Filename,1,102);
+   subStr = extractBetween(subStr,"_",".csv");
+   scan = str2num(subStr{1});
    allVarNames = T.Properties.VariableNames;
    
    % find stimulus arrival
@@ -26,31 +31,31 @@ for ii = 1:52
    tEnd = table2array(arrivals(:,1));
    tStart = table2array(start(:,1));
    
-   if ismember(ii, [3 8 13 24 25 29 34 38 50 51])
+   if ismember(scan, [3 8 13 24 25 29 34 38 50 51])
        % 3.5 PSI
        for jj = 1:length(tEnd)
            x(end+1) = 3.5;
            y(end+1) = tEnd(jj) - tStart(jj);
        end
-   elseif ismember(ii, [9 11 12 20 22 35 37 38 46 48])
+   elseif ismember(scan, [9 11 12 20 22 35 37 38 46 48])
        % 7.5 PSI       
        for jj = 1:length(tEnd)
            x(end+1) = 7.5;
            y(end+1) = tEnd(jj) - tStart(jj);
        end
-   elseif ismember(ii, [4 7 16 17 21 30 33 42 43 47])
+   elseif ismember(scan, [4 7 16 17 21 30 33 42 43 47])
        % 15 PSI
        for jj = 1:length(tEnd)
            x(end+1) = 15;
            y(end+1) = tEnd(jj) - tStart(jj);
        end
-   elseif ismember(ii, [2 10 15 18 26 28 36 41 44 52])
+   elseif ismember(scan, [2 10 15 18 26 28 36 41 44 52])
        % 30 PSI
        for jj = 1:length(tEnd)
            x(end+1) = 30;
            y(end+1) = tEnd(jj) - tStart(jj);
        end
-   else
+   elseif ismember(scan, [5 6 14 19 23 31 32 40 45 49])
        % 60 PSI
        for jj = 1:length(tEnd)
            x(end+1) = 60;
