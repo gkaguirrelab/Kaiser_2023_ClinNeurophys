9392ed602b4f9359d35d9ef3ecc06e3a5b081a75
gkaguirre <gkaguirre@me.com>
2022-06-08 15:13:19 +0200

    Another figure

--- code/primaryBlinkShapeAnalysis.m
+++ code/primaryBlinkShapeAnalysis.m
@@ -61,11 +61,19 @@ nTrials = zeros(nSubs,5);
 % Loop through subjects and pressure levels. Load the full and by-session
 % data separately for ease of coding below
 for ss=1:nSubs
-    for pp=1:5
+    for pp=1:nPSIs
         [X(ss,pp,:),~,nTrials(ss,pp)]=returnBlinkTimeSeries( subjectIDs{ss}, targetPSISet(pp));
         X1(ss,pp,:)=returnBlinkTimeSeries( subjectIDs{ss}, targetPSISet(pp), 1);
         X2(ss,pp,:)=returnBlinkTimeSeries( subjectIDs{ss}, targetPSISet(pp), 2);
     end
+
+    % Get the raw vector and obtain the blinks averaged by trial index for
+    % the analysis of habituation effects
+    [~,~,~,blinkVectorRaw,trialIndices] = returnBlinkTimeSeries( subjectIDs{ss} );
+    for tt=1:nBlinksPerAcq
+        trialX(ss,tt,:) = nanmean(blinkVectorRaw(trialIndices==tt,:));
+    end
+
 end
 
 % Call the function once more to grab the temporal support
@@ -76,6 +84,7 @@ end
 X_ICA = reshape(X,nSubs*nPSIs,nTimePoints);
 X1_ICA = reshape(X1,nSubs*nPSIs,nTimePoints);
 X2_ICA = reshape(X2,nSubs*nPSIs,nTimePoints);
+trialX_ICA = reshape(trialX,nSubs*nBlinksPerAcq,nTimePoints);
 
 % Remove any "bad" blink averages that are all nans
 goodIdx = ~any(isnan(X_ICA'));
@@ -98,6 +107,7 @@ Mdl = rica(X_ICA,q);
 X_ICAcoeff = Mdl.transform(X_ICA);
 X1_ICAcoeff = Mdl.transform(X1_ICA);
 X2_ICAcoeff = Mdl.transform(X2_ICA);
+trialX_ICAcoeff = Mdl.transform(trialX_ICA);
 
 % Extract the components
 components = Mdl.TransformWeights;
@@ -115,6 +125,8 @@ X1coeff = reshape(X1coeff,nSubs,nPSIs,q);
 X2coeff = nan(nSubs*nPSIs,q);
 X2coeff(goodIdx,:)=X2_ICAcoeff;
 X2coeff = reshape(X2coeff,nSubs,nPSIs,q);
+trialXcoeff = reshape(trialX_ICAcoeff,nSubs,nBlinksPerAcq,q);
+
 Xfit = nan(nSubs*nPSIs,nTimePoints);
 Xfit(goodIdx,:) = X_ICAfit';
 Xfit = reshape(Xfit,nSubs,nPSIs,nTimePoints);
@@ -134,6 +146,7 @@ end
 %% FIGURES
 %%%%%%%%%%%%%%%%%%%%%
 
+
 % Define a gray-to-red color set for puff-pressure
 psiColors = [0.5:0.125:1.0; 0.5:-0.125:0; 0.5:-0.125:0]';
 
@@ -141,13 +154,13 @@ psiColors = [0.5:0.125:1.0; 0.5:-0.125:0; 0.5:-0.125:0]';
 %% Acquisition order and example raw set of blinks
 psiAcqOrder = [4 4 1 3 5 5 3 1 2 4 2 2 1 5 4 3 3 4 5 2 3 2 5 1 1 4];
 figure
-set(gcf, 'Position',  [100, 100, 560, 200])
+set(gcf, 'Position',  [100, 100, 400, 200])
 subplot(3,2,1:2)
 plotIdx = 1;
 dash = 10;
 dot = 2;
 for aa=1:length(psiAcqOrder)
-  plot( plotIdx:plotIdx+dash-1, repmat(psiAcqOrder(aa),dash,1), '-', 'Color', psiColors(psiAcqOrder(aa),:),'LineWidth',2)
+  plot( plotIdx:plotIdx+dash-1, repmat(psiAcqOrder(aa),dash,1), '-', 'Color', psiColors(psiAcqOrder(aa),:),'LineWidth',4)
   if aa==1; hold on; end
   plotIdx = plotIdx+dash+dot;
 end
@@ -187,10 +200,10 @@ for ss=1:2
     plot([0 0],[-125 25],'-b')
     title(sprintf('Session %d',ss));
     if ss==1
-    plot([-100 -100],[0 -125],'-','Color',[0.5 0.5 0.5],'LineWidth',2)    
+    plot([-100 -100],[0 -100],'-','Color',[0.5 0.5 0.5],'LineWidth',2)    
     plot([-100 0],[-125 -125],'-','Color',[0.5 0.5 0.5],'LineWidth',2)
     t=text(-100,-50,'100 msec');
-    t=text(-175,-100,'125 pixels');
+    t=text(-175,-100,'100 pixels');
     t.Rotation = 90;
     end
 end
@@ -214,7 +227,7 @@ saveas(gcf,fullfile(plotSaveDir,'averageBlnkResponseByPSI.pdf'));
 %% Illustration of the ICA components
 figure
 componentNames = {'amplitude','shape1','shape2','speed'};
-componentColors = [0 0 0; 0.85 0.85 0.85; 0.65 0.65 0.65; 0 0 1];
+componentColors = [0 0 0; 0.65 0.65 0.65; 0.85 0.85 0.85; 0 0 1];
 componentWidths = [1.5, 1, 1, 1.5];
 plotOrder = [1 4 2 3];
 for cc = plotOrder
@@ -227,31 +240,39 @@ ylabel('component value [a.u.]');
 saveas(gcf,fullfile(plotSaveDir,'ICAcomponents.pdf'));
 
 
-% Plot of the coefficients by puff pressure
+%% Plot of the coefficients by puff pressure
 figure
 meanCoeff = squeeze(mean(Xcoeff,1));
 semCoeff = squeeze(std(Xcoeff,1))./sqrt(nSubs);
+plotOrder = [1 3 4 2];
 for cc=1:4
-    subplot(2,2,cc)
+    subplot(2,2,plotOrder(cc))
     for pp = 1:nPSIs
-        plot([log10(targetPSISet(pp)) log10(targetPSISet(pp))],[meanCoeff(pp,cc)+semCoeff(:,cc),meanCoeff(pp,cc)-semCoeff(:,cc)],'-k');
+        plot([log10(targetPSISet(pp)) log10(targetPSISet(pp))],[meanCoeff(pp,cc)+2.*semCoeff(:,cc),meanCoeff(pp,cc)-2.*semCoeff(:,cc)],'-k');
         hold on
-        plot(log10(targetPSISet(pp)),meanCoeff(pp,cc),'o','Color',componentColors(cc,:));
+        plot(log10(targetPSISet(pp)),meanCoeff(pp,cc),'o',...
+            'MarkerFaceColor',componentColors(cc,:),'MarkerEdgeColor','none' );
     end
     % Add a linear fit line
     if cc==4
         pp = polyfit(xVals(2:end),meanCoeff(2:end,cc),1);
         plot([xVals(2)+xValMid xVals(end)+xValMid],polyval(pp,[xVals(2) xVals(end)]),'-r')
         plot([xVals(1)+xValMid xVals(2)+xValMid],polyval(pp,[xVals(2) xVals(2)]),'-r')
+        text(1,100,'±2SEM');
     else
         pp = polyfit(xVals,meanCoeff(:,cc),1);
         plot([xVals(1)+xValMid xVals(end)+xValMid],polyval(pp,[xVals(1) xVals(end)]),'-r')
     end
+    xticks(log10(targetPSISet));
+    xticklabels(arrayfun(@num2str, targetPSISet, 'UniformOutput', 0));
+    xlabel('stimulus intensity [log PSI]')
+    title(componentNames{cc})
+    box off
 end
 saveas(gcf,fullfile(plotSaveDir,'coefficientsByPSI.pdf'));
 
 
-% Calculate the correlation of the fit with each average blink response
+%% Calculate the correlation of the fit with each average blink response
 for ss=1:nSubs
     for pp=1:5
         varExplained(ss,pp) = corr(squeeze(X(ss,pp,:)),squeeze(Xfit(ss,pp,:)))^2';
@@ -259,7 +280,56 @@ for ss=1:nSubs
 end
 fprintf('The mean [range] of R^2 of model fit to blink is %2.2f [%2.2f - %2.2f]\n',mean(varExplained(:)),min(varExplained(:)),max(varExplained(:)))
 
-% Show scatter plots of test / retest of overall amplitude and speed
+
+%% Show scatter plots of across-session amplitude and speed
+limVals = {...
+    [0 700],[-100 300];...
+    [0 1000],[-150 150]};
+subIdxToShow = {[8,13],[16,9]}; % [14, 13]
+symbolColors={'g','g'};
+nameRow = {'slope','offset'};
+axisLabels = {'amplitude','velocity'};
+figure
+
+for rr=1:2
+    subplot(2,3,(2-rr)*3+1);
+    vals1 = ampPuffCoeff(:,rr);
+    vals2 = speedPuffCoeff(:,rr);
+    scatter(vals1,vals2,'MarkerFaceColor',[0.25 0.25 0.25],'MarkerEdgeColor','none','MarkerFaceAlpha',0.5);
+    xlim(limVals{rr,1}); ylim(limVals{rr,2});
+    xlabel([axisLabels{1} ' ' nameRow{rr} ' [a.u.]']);
+    ylabel([axisLabels{2} ' ' nameRow{rr} ' [a.u.]']);
+    axis square; box off
+    titleStr = nameRow{rr};
+    title(titleStr);
+    hold on
+    for ss=1:length(subIdxToShow{rr})
+        scatter(vals1(subIdxToShow{rr}(ss)),vals2(subIdxToShow{rr}(ss)),200,'r','MarkerFaceColor','none');
+    end
+    if rr==2
+        pressureToPlotIdx = [0 0 1 0 0];
+    else
+        pressureToPlotIdx = [1 1 1 1 1];
+    end
+    for ss=1:length(subIdxToShow{rr})
+        subplot(2,3,(2-rr)*3+1+ss);
+        for pp=1:length(pressureToPlotIdx)
+            if pressureToPlotIdx(pp)==1
+                plot(temporalSupport, returnBlinkTimeSeries( subjectIDs{subIdxToShow{rr}(ss)}, targetPSISet(pp) ), '-', 'Color', psiColors(pp,:),'LineWidth',1);
+                hold on
+            end
+        end
+        ylim([-150 25]);
+        plot([-100 -100],[0 -100],'-','Color',[0.5 0.5 0.5],'LineWidth',2)
+        plot([-100 0],[-125 -125],'-','Color',[0.5 0.5 0.5],'LineWidth',2)
+        axis off
+    end
+end
+saveas(gcf,fullfile(plotSaveDir,'subjectCoeffDistribution.pdf'));
+
+
+
+%% Show scatter plots of test / retest of overall amplitude and speed
 limVals = {...
     [0 700],[-100 300];...
     [0 1000],[-150 150]};
@@ -289,12 +359,12 @@ saveas(gcf,fullfile(plotSaveDir,'testRetestCoefficients.pdf'));
 
 
 %% Illustration of all blink responses and ICA model fit
-figure
 betweenSubGap = 1;
 grayScaleRangePixels = [50 -200];
 
 % Loop over the three display panels
 for xx = 1:3
+        figure
     switch xx
         case 1
             % Create uniC, which is permuted to order rows by subject then puff
@@ -329,17 +399,57 @@ for xx = 1:3
         C(CrowStart:CrowStart+nPSIs-1,11:end,:) = repmat(uniC(XrowStart:XrowStart+4,:),1,1,3);
 
         % Add a color bar
-        C(CrowStart:CrowStart+nPSIs-1,1:10,:) = permute(repmat(psiColors,1,1,10),[1 3 2]);
+        C(CrowStart:CrowStart+nPSIs-1,1:7,:) = permute(repmat(psiColors,1,1,7),[1 3 2]);
 
         % Add a marker for stimulus onset
         C(CrowStart:CrowStart+nPSIs-1,10+zeroIdx,:) = repmat([0 0 1],nPSIs,1);
 
     end
 
-    subplot(1,3,xx);
     image(imresize(C,[size(C,1)*4,size(C,2)],"nearest")); axis off
     axis equal
     title(titleStr);
+    saveas(gcf,fullfile(plotSaveDir,sprintf('blinkAndFitAllSubjects_%d.png',xx)));
 end
-saveas(gcf,fullfile(plotSaveDir,'blinkAndFitAllSubjects.png'));
+
+
+%% Evoked responses from subjects with high and low sensitivity
+
+figure
+subIdxToPlot = [14,12,3,1];
+for ss = 1:length(subIdxToPlot)
+    subplot(1,length(subIdxToPlot),ss)
+    for pp=1:nPSIs
+        plot(temporalSupport, returnBlinkTimeSeries( subjectIDs{subIdxToPlot(ss)}, targetPSISet(pp) ), '-', 'Color', psiColors(pp,:),'LineWidth',0.5);
+        if pp==1;hold on; end
+    end
+    axis off
+    ylim([-175 20])
+    plot([-100 -100],[0 -100],'-','Color',[0.5 0.5 0.5],'LineWidth',2)
+    title(sprintf('slopes [%2.0f,%2.0f]',ampPuffCoeff(subIdxToPlot(ss),1),speedPuffCoeff(subIdxToPlot(ss),1)))
+end
+
+
+%% Supplementary figure: Reconstruction error with fewer components
+
+% Calculate the variance explained as a function of dimensionality
+figure
+for qq=1:6
+    subplot(3,3,qq)
+    tmpMdl = rica(X_ICA,qq);
+    tmpFit = (tmpMdl.TransformWeights*tmpMdl.transform(X_ICA)')';
+    tmpFitX = reshape(tmpFit,nSubs,nPSIs,nTimePoints);
+    tmpXfit = squeeze(mean(tmpFitX,1));
+    rSquaredByDimension = corr(X_ICA(:),tmpFit(:))^2;
+    tmpX = squeeze(mean(X,1));
+    for pp = 1:nPSIs
+        plot(temporalSupport,tmpX(pp,:),'-','Color',psiColors(pp,:),'LineWidth',0.5)
+        hold on
+        plot(temporalSupport,tmpXfit(pp,:),'--','Color',psiColors(pp,:),'LineWidth',1)
+    end
+    xlabel('time [msecs]');
+    ylabel('blink depth [pixels]');
+    title(sprintf('q=%d, R^2=%2.2f',qq,rSquaredByDimension))
+end
+saveas(gcf,fullfile(plotSaveDir,'Supp_variationInICAq.pdf'));
 
